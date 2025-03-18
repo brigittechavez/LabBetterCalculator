21c1184fcfe6c53f1a9ec3f2be55b969cce82a1e
brigittechavez <brigitte.chavez@correounivalle.edu.co>
2025-03-18 09:44:19 -0500

    Funciones básicas

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
+def addmultiplenumbers (numbers):
+
+  total = 0
+  for num in numbers:
+    total += num
+
+    return total
+  
+def multiplymultiplenumbers (numbers):
+
+  total = 1
+  for num in numbers:
+    total *= num
+
+    return total
+  
+
+def isiteven(num):
+  if num % 2 == 0:
+    return True
+  else:
+    return False
+
+
+
 def main():
   print("Hello learners!")
+
+      
 
 if __name__=="__main__":
   main()
